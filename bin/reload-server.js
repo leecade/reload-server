19748d05b4ed2d1eab3fa1bb0f675d7fe8f747ab
lepture <lepture@me.com>
2012-08-08 15:31:16 +0800

    binary available

--- bin/reload-server.js
+++ bin/reload-server.js
@@ -1,1 +1,44 @@
 #!/usr/bin/env node
+
+var server = require('../')
+
+function main(argv) {
+  var path = '.', port = 8000
+
+  function getArg() {
+    var args = argv.shift()
+    args = args.split('=')
+    if (args.length > 1) {
+      argv.unshift(args.slice(1).join('='))
+    }
+    return args[0]
+  }
+
+  var arg
+  while (argv.length) {
+    arg = getArg()
+    switch(arg) {
+      case '-p':
+      case '--port':
+        port = argv.shift()
+        break
+      case '-h':
+      case '--help':
+        //TODO
+        process.exit(0);
+        break
+      default:
+        path = arg
+        break
+    }
+  }
+
+  if (path == process.argv[1]) {
+    path = '.'
+  }
+
+  port = parseInt(port, 10)
+  server.watch(path, port)
+}
+
+main(process.argv.slice())
